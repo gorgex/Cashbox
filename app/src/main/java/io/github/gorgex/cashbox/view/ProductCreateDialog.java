f8ae00f29855c29238bf21a70fdc0b59d0560e1f
gorgex <gio.pogosyan@gmail.com>
2020-01-31 18:42:15 +0400

    dialog fix

--- app/src/main/java/io/github/gorgex/cashbox/view/ProductCreateDialog.java
+++ app/src/main/java/io/github/gorgex/cashbox/view/ProductCreateDialog.java
@@ -2,67 +2,149 @@ package io.github.gorgex.cashbox.view;
 
 import androidx.appcompat.app.AlertDialog;
 
+import android.annotation.SuppressLint;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.os.Build;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatDialogFragment;
 
+import com.google.android.material.textfield.TextInputLayout;
+
+import java.text.NumberFormat;
+
 import io.github.gorgex.cashbox.R;
 
 public class ProductCreateDialog extends AppCompatDialogFragment {
 
+    private TextInputLayout nameInputLayout;
     private EditText productName;
     private EditText productPrice;
     private EditText productQuantity;
+    private Button save;
     private ProductCreateDialogListener listener;
+    private InputMethodManager inputMethodManager;
 
+    @SuppressLint("ClickableViewAccessibility")
     @NonNull
     @Override
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         LayoutInflater inflater = getActivity().getLayoutInflater();
         View view = inflater.inflate(R.layout.dialog_create_product, null);
 
-        builder.setView(view)
-                .setTitle("Create a Product")
-                .setPositiveButton("Save", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        String name = productName.getText().toString();
-                        String price = productPrice.getText().toString();
-                        String quantity = productQuantity.getText().toString();
-                        double p;
-                        double q;
-                        if (price.isEmpty()) {
-                            p = 0;
-                        } else {
-                            p = Double.parseDouble(price);
-                        }
-                        if (quantity.isEmpty()) {
-                            q = 0;
-                        } else {
-                            q = Double.parseDouble(quantity);
-                        }
-                        if (!name.isEmpty()) {
-                            listener.createProduct(name, p, q);
-                        } else {
-                            dialog.dismiss();
-                        }
-                    }
-                });
-
+        nameInputLayout = view.findViewById(R.id.nameInputLayout);
         productName = view.findViewById(R.id.product_name);
         productPrice = view.findViewById(R.id.product_price);
         productQuantity = view.findViewById(R.id.product_quantity);
+        inputMethodManager = (InputMethodManager) view.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
 
-        return builder.create();
+        builder.setView(view)
+                .setTitle("Create a Product")
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        inputMethodManager.toggleSoftInput(InputMethodManager.HIDE_IMPLICIT_ONLY, 0);
+                    }
+                });
+        builder.setPositiveButton("Save", null);
+        productName.requestFocus();
+        inputMethodManager.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0);
+
+        if (Build.VERSION.SDK_INT > 26) {
+            productName.setImportantForAutofill(View.IMPORTANT_FOR_AUTOFILL_NO);
+        }
+
+        productPrice.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                if (productName.getText().toString().isEmpty()) {
+                    nameInputLayout.setError("Invalid name");
+                }
+                return false;
+            }
+        });
+
+        productQuantity.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                if (productName.getText().toString().isEmpty()) {
+                    nameInputLayout.setError("Invalid name");
+                }
+                return false;
+            }
+        });
+
+        final AlertDialog dialog = builder.create();
+        dialog.show();
+        dialog.setCanceledOnTouchOutside(false);
+        save = dialog.getButton(DialogInterface.BUTTON_POSITIVE);
+        save.setEnabled(false);
+
+        productName.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                nameInputLayout.setError(null);
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if (editable.toString().isEmpty()) {
+                    nameInputLayout.setError("Invalid name");
+                    save.setEnabled(false);
+                } else {
+                    save.setEnabled(true);
+                }
+            }
+        });
+
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = productName.getText().toString();
+                String price = productPrice.getText().toString();
+                String quantity = productQuantity.getText().toString();
+                double p;
+                double q;
+                if (price.isEmpty()) {
+                    p = 0;
+                } else {
+                    p = Double.parseDouble(price);
+                }
+                if (quantity.isEmpty()) {
+                    q = 0;
+                } else {
+                    q = Double.parseDouble(quantity);
+                }
+                if (name.isEmpty()) {
+                    nameInputLayout.setError("Invalid name");
+                } else {
+                    listener.createProduct(name, p, q);
+                    inputMethodManager.toggleSoftInput(InputMethodManager.HIDE_IMPLICIT_ONLY, 0);
+                    dialog.dismiss();
+                }
+            }
+        });
+
+        return dialog;
     }
 
     @Override
--- app/src/main/java/io/github/gorgex/cashbox/view/ProductEditDialog.java
+++ app/src/main/java/io/github/gorgex/cashbox/view/ProductEditDialog.java
@@ -1,17 +1,27 @@
 package io.github.gorgex.cashbox.view;
 
+import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.os.Build;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatDialogFragment;
+
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputLayout;
 
 import java.text.NumberFormat;
 
@@ -19,10 +29,13 @@ import io.github.gorgex.cashbox.R;
 
 public class ProductEditDialog extends AppCompatDialogFragment {
 
+    private TextInputLayout nameInputLayout;
     private EditText productName;
     private EditText productPrice;
     private EditText productQuantity;
+    private Button save;
     private ProductEditDialogListener listener;
+    private InputMethodManager inputMethodManager;
 
     private String name;
     private double price;
@@ -41,41 +54,90 @@ public class ProductEditDialog extends AppCompatDialogFragment {
         LayoutInflater inflater = getActivity().getLayoutInflater();
         View view = inflater.inflate(R.layout.dialog_create_product, null);
 
-        builder.setView(view)
-                .setTitle("Edit Product")
-                .setPositiveButton("Save", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        String n = productName.getText().toString();
-                        String p = productPrice.getText().toString();
-                        String q = productQuantity.getText().toString();
-                        if (p.isEmpty()) {
-                            price = 0;
-                        } else {
-                            price = Double.parseDouble(p);
-                        }
-                        if (q.isEmpty()) {
-                            quantity = 0;
-                        } else {
-                            quantity = Double.parseDouble(q);
-                        }
-                        if (!n.isEmpty()) {
-                            listener.editProduct(n, price, quantity);
-                        } else {
-                            dialogInterface.dismiss();
-                        }
-                    }
-                });
-
+        nameInputLayout = view.findViewById(R.id.nameInputLayout);
         productName = view.findViewById(R.id.product_name);
         productPrice = view.findViewById(R.id.product_price);
         productQuantity = view.findViewById(R.id.product_quantity);
+        inputMethodManager = (InputMethodManager) view.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
 
         productName.setText(name);
         productPrice.setText(NumberFormat.getInstance().format(price));
         productQuantity.setText(NumberFormat.getInstance().format(quantity));
 
-        return builder.create();
+        builder.setView(view)
+                .setTitle("Edit Product")
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        inputMethodManager.toggleSoftInput(InputMethodManager.HIDE_IMPLICIT_ONLY, 0);
+                    }
+                });
+
+        builder.setPositiveButton("Save", null);
+        productName.requestFocus();
+        inputMethodManager.toggleSoftInput(InputMethodManager.SHOW_FORCED, 0);
+
+        if (Build.VERSION.SDK_INT > 26) {
+            productName.setImportantForAutofill(View.IMPORTANT_FOR_AUTOFILL_NO);
+        }
+
+        final AlertDialog dialog = builder.create();
+        dialog.show();
+        dialog.setCanceledOnTouchOutside(false);
+        save = dialog.getButton(DialogInterface.BUTTON_POSITIVE);
+//        Button cancel = dialog.getButton(DialogInterface.BUTTON_NEGATIVE);
+//        cancel.setTextColor(getResources().getColor(R.color.colorPrimary));
+//        cancel.setBackgroundColor(getResources().getColor(android.R.color.transparent));
+
+        productName.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                nameInputLayout.setError(null);
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if (editable.toString().isEmpty()) {
+                    nameInputLayout.setError("Invalid name");
+                    save.setEnabled(false);
+                } else {
+                    save.setEnabled(true);
+                }
+            }
+        });
+
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String n = productName.getText().toString();
+                String p = productPrice.getText().toString();
+                String q = productQuantity.getText().toString();
+                if (p.isEmpty()) {
+                    price = 0;
+                } else {
+                    price = Double.parseDouble(p);
+                }
+                if (q.isEmpty()) {
+                    quantity = 0;
+                } else {
+                    quantity = Double.parseDouble(q);
+                }
+                if (n.isEmpty()) {
+                    nameInputLayout.setError("Invalid name");
+                } else {
+                    listener.editProduct(n, price, quantity);
+                    inputMethodManager.toggleSoftInput(InputMethodManager.HIDE_IMPLICIT_ONLY, 0);
+                    dialog.dismiss();
+                }
+            }
+        });
+
+        return dialog;
     }
 
     @Override
